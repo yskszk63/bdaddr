120b66b189c7e9eb655eb04c89ddb585cb4faf7e
yskszk63 <yskszk63@gmail.com>
2021-11-18 09:37:35 +0900

    Include README.md

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+[![Crates.io](https://img.shields.io/crates/v/bdaddr)](https://crates.io/crates/bdaddr)
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/yskszk63/bdaddr/CI)](https://github.com/yskszk63/bdaddr/actions/workflows/ci.yml)
+[![Libraries.io dependency status for GitHub repo](https://img.shields.io/librariesio/github/yskszk63/bdaddr)](https://libraries.io/cargo/bdaddr)
+![Codecov](https://img.shields.io/codecov/c/gh/yskszk63/bdaddr)
+
 # bdaddr
 
 Bluetooth Device Address.
@@ -12,11 +17,12 @@ bdaddr = "0.1"
 ### Example
 
 ```rust
-use bdaddr::Address;
+use bdaddr::{Address, RandomDeviceAddress, StaticDeviceAddress};
 
 fn main() {
-    let addr = "00:11:22:33:44:55".parse::<Address>().unwrap();
+    let addr = Address::le_random_from_str("00:11:22:33:44:55").unwrap();
     assert_eq!(addr.to_string(), "00:11:22:33:44:55");
+    assert!(matches!(addr, Address::LeRandom(RandomDeviceAddress::NonResolvable(..))));
 }
 ```
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,5 @@
 #![doc(html_root_url = "https://docs.rs/bdaddr/0.1.2")]
-//! Bluetooth Device Address.
-//!
-//! ## Dependencies
-//!
-//! ```toml
-//! [dependencies]
-//! bdaddr = "0.1"
-//! ```
-//!
-//! ## Example
-//!
-//! ```
-//! use bdaddr::{Address, RandomDeviceAddress, StaticDeviceAddress};
-//!
-//! fn main() {
-//!     let addr = Address::le_random_from_str("00:11:22:33:44:55").unwrap();
-//!     assert_eq!(addr.to_string(), "00:11:22:33:44:55");
-//!     assert!(matches!(addr, Address::LeRandom(RandomDeviceAddress::NonResolvable(..))));
-//! }
-//! ```
-//!
-//! ## License
-//!
-//! Licensed under either of
-//! * Apache License, Version 2.0
-//!   ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
-//! * MIT license
-//!   ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
-//! at your option.
-//!
-//! ## Contribution
-//!
-//! Unless you explicitly state otherwise, any contribution intentionally submitted
-//! for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
-//! dual licensed as above, without any additional terms or conditions.!
+#![doc = include_str!("../README.md")]
 pub use addr::{
     Address, AddressParseError, NonResolvablePrivateAddress, PublicDeviceAddress,
     RandomDeviceAddress, ResolvablePrivateAddress, StaticDeviceAddress,
